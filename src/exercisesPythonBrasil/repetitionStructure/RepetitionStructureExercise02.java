4a34bd2f319c74158c2fed4132b77aee8bd2e874
Monique Hamatsu <mkhamatsu@outlook.com>
2022-07-19 19:17:41 -0300

    ExerciciosPythonBrasil/Estrutura De Repetição: exercício 04

--- src/exercisesPythonBrasil/repetitionStructure/RepetitionStructureExercise02.java
+++ src/exercisesPythonBrasil/repetitionStructure/RepetitionStructureExercise02.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /*
  Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário,
  mostrando uma mensagem de erro e voltando a pedir as informações.
- */
+*/
 
 public class RepetitionStructureExercise02 {
 
---
+++ src/exercisesPythonBrasil/repetitionStructure/RepetitionStructureExercise04.java
@@ -0,0 +1,33 @@
+package exercisesPythonBrasil.repetitionStructure;
+
+/*
+ Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% 
+ e que a população de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. 
+ Faça um programa que calcule e escreva o número de anos necessários para que a população 
+ do país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento. 
+*/
+
+public class RepetitionStructureExercise04 {
+
+	public static void main(String[] args) {
+
+		int populationA = 80000;
+		int populationB = 200000;
+
+		double growthPercentageA = (1 + 0.03);
+		double growthPercentageB = (1 + 0.015);
+
+		int years = 0;
+
+		do {
+			years += 1;
+			populationA *= growthPercentageA;
+			populationB *= growthPercentageB;
+		} while (populationA <= populationB);
+
+		System.out.println("Population A: " + populationA);
+		System.out.println("Population B: " + populationB);
+		System.out.println("The populationA is equal or bigger than populationB in " + years + " years.");
+
+	}
+}
