e3752c36cbdbc5622bbcc3dede78c1ab585f810c
Monique Hamatsu <mkhamatsu@outlook.com>
2022-07-20 15:45:15 -0300

    ExerciciosPythonBrasil/Estrutura De Repetição: exercício 10

---
+++ src/exercisesPythonBrasil/repetitionStructure/RepetitionStructureExercises10.java
@@ -0,0 +1,47 @@
+package exercisesPythonBrasil.repetitionStructure;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+Faça um programa que receba dois números inteiros 
+e gere os números inteiros que estão no intervalo compreendido por eles
+*/
+
+public class RepetitionStructureExercises10 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		List<Integer> numbers = new ArrayList<>();
+
+		System.out.println("Type a number: ");
+		int num1 = scan.nextInt();
+		System.out.println("Type a number: ");
+		int num2 = scan.nextInt();
+
+		int firstNumber;
+		int lastNumber;
+
+		if (num1 > num2) {
+			firstNumber = num2;
+			lastNumber = num1;
+
+		} else {
+			firstNumber = num1;
+			lastNumber = num2;
+		}
+
+		while (firstNumber + 1 <= lastNumber - 1) {
+			firstNumber++;
+			numbers.add(firstNumber);
+		}
+
+		System.out.println(numbers);
+
+		scan.close();
+
+	}
+}
