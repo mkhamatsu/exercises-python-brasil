550de4faa6156edd42f7be5a86ba1a2241e5cf32
Monique Hamatsu <mkhamatsu@outlook.com>
2022-07-20 14:04:52 -0300

    ExerciciosPythonBrasil/Estrutura De Repetição: exercício 08

---
+++ src/exercisesPythonBrasil/repetitionStructure/RepetitionStructureExercise08.java
@@ -0,0 +1,34 @@
+package exercisesPythonBrasil.repetitionStructure;
+
+import java.util.Scanner;
+
+/*
+ Faça um programa que leia 5 números e informe a soma e a média dos números
+*/
+
+public class RepetitionStructureExercise08 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+		int count = 0;
+		int size = 5;
+
+		double sum = 0;
+
+		while (count < size) {
+			count++;
+			System.out.println("Type a number [" + count + "]: ");
+			sum += scan.nextInt();
+		}
+
+		double avarage = sum / size;
+
+		System.out.println("The sum of the values ​​is " + sum);
+		System.out.println("The average ​​is " + avarage);
+
+		scan.close();
+
+	}
+}
