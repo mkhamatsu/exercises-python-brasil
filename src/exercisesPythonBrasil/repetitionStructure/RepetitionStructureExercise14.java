f9043962f4f17c4213441d02d182394a8e6aa925
Monique Hamatsu <mkhamatsu@outlook.com>
2022-07-21 08:43:32 -0300

    ExerciciosPythonBrasil/Estrutura De Repetição: exercício 14

---
+++ src/exercisesPythonBrasil/repetitionStructure/RepetitionStructureExercise14.java
@@ -0,0 +1,40 @@
+package exercisesPythonBrasil.repetitionStructure;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ Faça um programa que peça 10 números inteiros, 
+ calcule e mostre a quantidade de números pares e a quantidade de números impares.
+*/
+
+public class RepetitionStructureExercise14 {
+
+	public static void main(String[] args) {
+
+		var evenNumbers = new ArrayList<>();
+		var oddNumbers = new ArrayList<>();
+		Scanner scan = new Scanner(System.in);
+		int count = 0;
+		int size = 10;
+		int inputNumber;
+
+		while (count < size) {
+			count++;
+			System.out.println("Type a number [" + count + "]: ");
+			inputNumber = scan.nextInt();
+			if (inputNumber % 2 == 0) {
+				evenNumbers.add(inputNumber);
+			} else {
+				oddNumbers.add(inputNumber);
+			}
+
+		}
+
+		System.out.println("Even numbers: " + evenNumbers.size());
+		System.out.println("The odd numbers: " + oddNumbers.size());
+
+		scan.close();
+
+	}
+}
